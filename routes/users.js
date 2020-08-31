8c930fe8f0cf4e3b128baaff9a484e0aac2fd4dc
Har-SHAW <8888harsha4444@gmail.com>
2020-09-01 00:10:02 +0530

    user router completed'

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@ var express = require("express");
 var router = express.Router();
 var User = require("../models/user");
 var passport = require("passport");
-var authenticate = require('../authenticate');
+var authenticate = require("../authenticate");
 
 router.get("/", (req, res, next) => {
   User.find({})
@@ -70,6 +70,50 @@ router.post("/login", passport.authenticate("local"), (req, res) => {
     success: true,
     token: token,
     status: "You are successfully logged in!",
+  });
+});
+
+router.put("/accept/:postId", authenticate.verifyUser, (req, res, next) => {
+  User.findById(req.user._id).then((data) => {
+    let acc = data.accepted;
+    let rej = data.rejected;
+    for (let i = 0; rej.length; i++) {
+      if (rej[i].equals(req.params.postId)) {
+        rej.splice(i, 1);
+      }
+    }
+    if (!acc.includes(req.params.postId)) {
+      acc.push(req.params.postId);
+    }
+    data.accepted = acc;
+    data.rejected = rej;
+    data.save().then((data) => {
+      res.statusCode = 200;
+      res.setHeader("Content-Type", "application/json");
+      res.json({ sucess: true });
+    });
+  });
+});
+
+router.put("/reject/:postId", authenticate.verifyUser, (req, res, next) => {
+  User.findById(req.user._id).then((data) => {
+    let rej = data.rejected;
+    let acc = data.accepted;
+    for (let i = 0; i < acc.length; i++) {
+      if (acc[i].equals(req.params.postId)) {
+        acc.splice(i, 1);
+      }
+    }
+    if (!rej.includes(req.params.postId)) {
+      rej.push(req.params.postId);
+    }
+    data.rejected = rej;
+    data.accepted = acc;
+    data.save().then((data) => {
+      res.statusCode = 200;
+      res.setHeader("Content-Type", "application/json");
+      res.json({ sucess: true });
+    });
   });
 });
 
