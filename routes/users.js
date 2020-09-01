ba20733d6905817fef48d380fd385b4838491261
Har-SHAW <8888harsha4444@gmail.com>
2020-09-01 14:21:06 +0530

    syncing with frontend

--- routes/users.js
+++ routes/users.js
@@ -28,7 +28,7 @@ router.get("/", (req, res, next) => {
 //     .catch((err) => next(err));
 // });
 
-router.post("/signup", (req, res, next) => {
+router.post("/employer/signup", (req, res, next) => {
   User.register(
     new User({ username: req.body.username }),
     req.body.password,
@@ -43,7 +43,41 @@ router.post("/signup", (req, res, next) => {
         user.accepted = [];
         user.rejected = [];
         if (req.body.organization) user.organization = req.body.organization;
-        if (req.body.candidate) user.candidate = req.body.candidate;
+        user.candidate = false
+        user.save((err, user) => {
+          if (err) {
+            res.statusCode = 500;
+            res.setHeader("Content-Type", "application/json");
+            res.json({ err: err });
+            return;
+          }
+          passport.authenticate("local")(req, res, () => {
+            res.statusCode = 200;
+            res.setHeader("Content-Type", "application/json");
+            res.json({ success: true, status: "Registration Successful!" });
+          });
+        });
+      }
+    }
+  );
+});
+
+router.post("/candidate/signup", (req, res, next) => {
+  User.register(
+    new User({ username: req.body.username }),
+    req.body.password,
+    (err, user) => {
+      if (err) {
+        res.statusCode = 500;
+        res.setHeader("Content-Type", "application/json");
+        res.json({ err: err });
+      } else {
+        if (req.body.firstname) user.firstname = req.body.firstname;
+        if (req.body.lastname) user.lastname = req.body.lastname;
+        user.accepted = [];
+        user.rejected = [];
+        if (req.body.organization) user.organization = req.body.organization;
+        user.candidate = true
         user.save((err, user) => {
           if (err) {
             res.statusCode = 500;
